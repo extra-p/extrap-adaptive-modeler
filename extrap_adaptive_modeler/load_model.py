25186a741345d9a5a9552002d4371728a3a7d168
Alexander Geiß <alexander.geiss1@tu-darmstadt.de>
2024-06-14 17:26:02 +0200

    Fix broken import

--- extrap_adaptive_modeler/load_model.py
+++ extrap_adaptive_modeler/load_model.py
@@ -6,12 +6,13 @@
 # See the LICENSE file in the base directory for details.
 
 
-import importlib_resources
 import os
 from pathlib import Path
-from extrapadaptivemodeler.modeler.data_generator import TrainingDataGenerator
+
+import importlib_resources
 from extrap.entities.terms import CompoundTerm
 
+from extrap_adaptive_modeler.data_generator import TrainingDataGenerator
 
 _ML_MODEL_NAME = 'normalizedTerms_newPoints'
 
@@ -27,10 +28,11 @@ _TERMS = [CompoundTerm.create(*expo) for expo in
            (11, 4, 0), (3, 1, 0), (3, 1, 1)]]
 
 
-def get_model(bucket_indices, noise_category, positions, tf, retrain_epochs, retrain_examples_per_class, _cached_mlmodels):
+def get_model(bucket_indices, noise_category, positions, tf, retrain_epochs, retrain_examples_per_class,
+              _cached_mlmodels):
     ml_model_key = (noise_category, tuple(positions))
     if ml_model_key not in _cached_mlmodels:
-        ml_model_path = os.path.join(os.path.dirname(__file__), _ML_MODEL_NAME+'.h5')
+        ml_model_path = os.path.join(os.path.dirname(__file__), _ML_MODEL_NAME + '.h5')
         with importlib_resources.as_file(Path(ml_model_path)) \
                 as model_file:
             ml_model = tf.keras.models.load_model(model_file)
